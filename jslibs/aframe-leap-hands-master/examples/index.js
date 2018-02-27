2d4e5364e3e49220ec6eb7602a0325aadbaf8d76
MasterFelix <felixatnus2013@gmail.com>
2018-02-27 13:54:32 +0800

    Remove js library

--- jslibs/aframe-leap-hands-master/examples/index.js
+++
@@ -1,5 +0,0 @@
-window.Leap = require('leapjs');
-window.AFRAME = require('aframe');
-require('aframe-extras').registerAll();
-
-require('../').registerAll();
--- jslibs/aframe-leap-hands-master/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'system': require('./src/leap-system'),
-  'leap-hand': require('./src/leap-hand'),
-  registerAll: function () {
-    AFRAME.registerSystem('leap', this.system);
-    AFRAME.registerComponent('leap-hand', this['leap-hand']);
-  }
-};
