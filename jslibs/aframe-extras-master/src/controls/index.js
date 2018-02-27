9e932e569f8c756c843a5800d136d7d75d82b68e
angelw22 <angelwong9510@gmail.com>
2018-02-27 13:08:13 +0800

    initialize

---
+++ jslibs/aframe-extras-master/src/controls/index.js
@@ -0,0 +1,7 @@
+require('./checkpoint-controls');
+require('./gamepad-controls');
+require('./hmd-controls');
+require('./keyboard-controls');
+require('./mouse-controls');
+require('./touch-controls');
+require('./universal-controls');
---
+++ jslibs/aframe-leap-hands-master/examples/index.js
@@ -0,0 +1,5 @@
+window.Leap = require('leapjs');
+window.AFRAME = require('aframe');
+require('aframe-extras').registerAll();
+
+require('../').registerAll();
